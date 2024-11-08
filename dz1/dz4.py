1fd174e41652317fa154586ee2c842b462771391
IgorRatnikov1 <igorratnikov814@gmail.com>
2024-11-08 16:47:43 +0000

    ir

---
+++ dz1/dz4.py
@@ -0,0 +1,16 @@
+number = int(input("введите номер дня недели"))
+match number:
+    case 1:
+        print("понедельник")
+    case 2:
+        print("вторник")
+    case 3:
+        print("среда")
+    case 4:
+        print("четверг")
+    case 5:
+        print("пятница")
+    case 6:
+        print("суббота")
+    case 7:
+        print("воскресение")
